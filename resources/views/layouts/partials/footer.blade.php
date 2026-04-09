35daa13c44219d7b8f9e99079fca8d2ff5c700e8
Muhammad-Ashfaq1 <mitf19e032@gmail.com>
2026-04-09 13:39:27 -0400

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,16 @@
+ <!-- Footer -->
+ <footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+      <div
+        class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+        <div class="text-body">
+          &#169;
+          <script>
+            document.write(new Date().getFullYear());
+          </script>
+          , made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="footer-link">M Ashfaq</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- / Footer -->
