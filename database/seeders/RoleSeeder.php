8e6c04731bde7b155eba5ee6e582c892ea4558c0
SadiaAnjum120 <sadiaanjum53654@gmail.com>
2026-04-10 10:36:22 -0700

    Signup flow feature

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $roles = [
+            'super_admin',
+            'tenant_admin',
+            'manager',
+            'cashier',
+            'technician',
+            'inventory_clerk',
+            'customer',
+        ];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate([
+                'name' => $role,
+                'guard_name' => 'web'
+            ]);
+        }
+    }
+}
