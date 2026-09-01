a620369f06751651debdd2d2000358eefe1fa1f7
SadiaAnjum120 <sadiaanjum53654@gmail.com>
2026-09-01 09:21:43 -0700

    Fix sub-category form error validation display

--- public/assets/js/tenant/e-com/subcategories.js
+++ public/assets/js/tenant/e-com/subcategories.js
@@ -57,9 +57,36 @@
     $selection.toggleClass('is-invalid', invalid);
   };
 
+  const fieldFeedback = function ($field) {
+    if ($field.hasClass('select2-hidden-accessible')) {
+      return $field.closest('.position-relative').find('.invalid-feedback').first();
+    }
+
+    return $field.siblings('.invalid-feedback').first();
+  };
+
+  const applyFieldError = function (field, message) {
+    if (!message) {
+      return;
+    }
+
+    const $field = $form.find('[name="' + field + '"]').first();
+
+    if (! $field.length) {
+      return;
+    }
+
+    $field.addClass('is-invalid');
+    fieldFeedback($field).text(message).addClass('d-block');
+
+    if ($field.hasClass('select2-hidden-accessible')) {
+      setSelect2ErrorState($field, true);
+    }
+  };
+
   const resetValidationState = function () {
     $form.find('.is-invalid').removeClass('is-invalid');
-    $form.find('.invalid-feedback').text('');
+    $form.find('.invalid-feedback').text('').removeClass('d-block');
     setSelect2ErrorState($formCategory, false);
   };
 
@@ -141,7 +168,8 @@
       }
     }).on('change', function () {
       setSelect2ErrorState($element, false);
-      $element.closest('.position-relative').find('.invalid-feedback').text('');
+      $element.removeClass('is-invalid');
+      fieldFeedback($element).text('').removeClass('d-block');
     });
   };
 
@@ -255,7 +283,10 @@
         }
       },
       errorElement: 'div',
-      errorClass: 'invalid-feedback',
+      errorClass: 'jquery-validate-error',
+      errorPlacement: function (error, element) {
+        applyFieldError(element.attr('name'), error.text());
+      },
       highlight: function (element) {
         const $element = $(element);
         $element.addClass('is-invalid');
@@ -267,27 +298,22 @@
       unhighlight: function (element) {
         const $element = $(element);
         $element.removeClass('is-invalid');
+        fieldFeedback($element).text('').removeClass('d-block');
 
         if ($element.hasClass('select2-hidden-accessible')) {
           setSelect2ErrorState($element, false);
         }
       },
-      errorPlacement: function (error, element) {
-        const $element = $(element);
+      success: function (label, element) {
+        const $field = $(element);
+        $field.removeClass('is-invalid');
+        fieldFeedback($field).text('').removeClass('d-block');
 
-        if ($element.hasClass('select2-hidden-accessible')) {
-          $element.closest('.position-relative').find('.invalid-feedback').first().text(error.text());
-          return;
+        if ($field.hasClass('select2-hidden-accessible')) {
+          setSelect2ErrorState($field, false);
         }
 
-        const $feedback = $element.siblings('.invalid-feedback').first();
-
-        if ($feedback.length) {
-          $feedback.text(error.text());
-          return;
-        }
-
-        error.insertAfter(element);
+        label.remove();
       }
     });
   };
@@ -493,16 +519,22 @@
         })
         .fail(function (xhr) {
           if (xhr.status === 422 && xhr.responseJSON && xhr.responseJSON.errors) {
-            if (xhr.responseJSON.errors.id) {
-              showAlert('error', xhr.responseJSON.errors.id[0]);
-            }
+            const errors = xhr.responseJSON.errors;
+
+            Object.entries(errors).forEach(function (entry) {
+              applyFieldError(entry[0], entry[1][0]);
+            });
 
             if (validator) {
               validator.showErrors(Object.fromEntries(
-                Object.entries(xhr.responseJSON.errors).map(function (entry) {
+                Object.entries(errors).map(function (entry) {
                   return [entry[0], entry[1][0]];
                 })
               ));
+            }
+
+            if (errors.id) {
+              showAlert('error', errors.id[0]);
             }
 
             return;
